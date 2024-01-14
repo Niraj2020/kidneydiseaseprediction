96417a079611222d74316ccced9756e7e6a02e5a
Neeraj_K_Sharma_08 <nirajsharma2463@fmail.com>
2024-01-14 12:52:06 +0530

    files updated

---
+++ setup.py
@@ -0,0 +1,12 @@
+import sys
+import setuptools
+from setuptools import setup, find_packages
+
+setup(
+    name="Kidney Disease prediction",
+    version="0.0.3",
+    description="ML project",
+    author="Neeraj kumar sharma",
+    packages=find_packages(),
+    license="Apache License 2.0"
+)
